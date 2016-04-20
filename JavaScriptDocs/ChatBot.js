80b7595258d987157c5d72b74f4f6ffeab0c3046
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-04-20 14:09:57 -0700

    update with maps

---
+++ JavaScriptDocs/ChatBot.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Josh on 4/20/16.
+ * ChatBot for TravelBuddy App
+ */
+
+//selects random number between start and end
+function Randnum(start, end) {
+    return Math.floor((Math.random() * end) + start);
+}
