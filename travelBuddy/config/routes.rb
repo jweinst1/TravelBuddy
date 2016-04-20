ca51cccba2cad6d63976dfd51beaa27b17f86059
Guomin Zheng <ChristineZheng@calvisitor-10-105-139-123.calvisitor.1918.berkeley.edu>
2016-04-20 14:25:12 -0700

    travel

--- travelBuddy/config/routes.rb
+++ travelBuddy/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   post "users", to: "users#create"
+  get "trips", to: "trips#index"
 
   devise_for :users
   root to: "home#index"
