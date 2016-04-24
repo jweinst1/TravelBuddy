fa476f7949dd14ca1a708939ac9db4781b3b0307
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-04-23 23:37:21 -0700

    fixed

--- travelBuddy/config/routes.rb
+++ travelBuddy/config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
 
   resources :trips
   resources :locations
-<<<<<<< HEAD
-
-=======
->>>>>>> JenniferWenHsu/master
+  
   devise_for :users
   root to: "home#index"
 end
