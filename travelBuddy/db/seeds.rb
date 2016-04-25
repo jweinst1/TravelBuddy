ae0ee10b87fb4d1e773e9346efc7a28fc039a9dc
Jennifer Hsu <jennifer.wen123@gmail.com>
2016-04-24 22:50:59 -0700

    final

--- travelBuddy/db/seeds.rb
+++ travelBuddy/db/seeds.rb
@@ -13,3 +13,9 @@
 User.create!(email: 'papp@berkeley.edu', password: 'wrwfwfgw', name: 'Christine')
 User.create!(email: 'vt@berkeley.edu', password: 'password', name: 'Vicky')
 
+Trip.create!(name: 'North California', start: "11/25/2016", end: "11/26/2016")
+Trip.create!(name: 'Korea', start: "12/25/2016", end: "12/26/2016")
+
+Location.create!(name: 'Berkeley')
+Location.create!(name: 'Seoul')
+Location.create!(name: 'Bushan')
